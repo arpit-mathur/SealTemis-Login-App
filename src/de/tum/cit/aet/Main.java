b588063da72843cb23e12e7a3f0272b3027499fc
arpit <arpitmathur409@gmail.com>
2025-01-17 00:24:55 +0100

    presentable

--- src/de/tum/cit/aet/Main.java
+++ src/de/tum/cit/aet/Main.java
@@ -6,16 +6,19 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) {
+        /// Stage is application window itself. Scene contains visual contents (nodes)
         SealTemisApp app = new SealTemisApp(primaryStage);
         primaryStage.setTitle("SealTemis");
         primaryStage.getIcons().add(new Image("file:images/artemgus.png"));
-        primaryStage.setScene(new Scene(app.getLoginPage(), 600, 700));
+        primaryStage.setScene(new Scene(app.getLoginPage(), 600, 750));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+        ///launch() initializes the runtime, and creates the main application lifecycle
     }
 }
--- src/de/tum/cit/aet/SealTemisApp.java
+++ src/de/tum/cit/aet/SealTemisApp.java
@@ -23,7 +23,7 @@ public class SealTemisApp {
     private final Map<String, String> users = new HashMap<>();
     private String currentUser;
 
-    ///
+    /// attribute to count the number of failed attempts
     int failedLoginAttempts;
 
     public SealTemisApp(Stage stage) {
@@ -70,106 +70,133 @@ public class SealTemisApp {
         passwordField.setPromptText("Enter your password");
 
         Button loginButton = new Button("Login");
-       loginButton.setStyle("-fx-font-size: 20px;" +
+        loginButton.setStyle("-fx-font-size: 20px;" +
                "-fx-font-family: 'Comic Sans MS';" +
                "-fx-background-color: 'green';" +
-               "-fx-text-fill: 'white';");
+               "-fx-text-fill: 'white';"
+        );
 
 
         //Helpful CSS styling for the login page background:
         loginPageLayout.setStyle("-fx-background-image: url('file:images/NavyBlueSolid.png');"
                 + "-fx-background-size: cover;"
                 + "-fx-background-position: center center;"+
-                "-fx-padding: 70;");
+                "-fx-padding: 70;"
+        );
 
-        // TODO 5: Implement the scene change when the login button is clicked
 
+        /// error message will be displayed here (Invalid credentials)
         Label errorLabel = new Label();
         errorLabel.setTextFill(Color.RED);
         errorLabel.setFont(Font.font("Comic sans ms",FontWeight.SEMI_BOLD,18));
 
+        /// TODO 5: Login button Logic-->
 
-        loginButton.setOnAction(e -> {
-            String username1 = usernameField.getText();
-            String password1 = passwordField.getText();
-            if (users.containsKey(username1) && users.get(username1).equals(password1)) {
-                currentUser = username1;
-                stage.setScene(new Scene(getWelcomePage(), 600, 700));
-            } else {
-                errorLabel.setText("Invalid username or password.");
+        loginButton.setOnAction(action -> {
+            /// If the credentials are valid, change the scene to welcome page -->
+            if(users.containsKey(usernameField.getText()) && users.get(usernameField.getText())
+                    .equals(passwordField.getText()) && failedLoginAttempts < 3){
+                currentUser = usernameField.getText();
+                stage.setScene(new Scene(getWelcomePage(),600,750));
+            }
+            ///  If the credentials are Invalid -->
+            else {
+                failedLoginAttempts++;
+                /// Based on number of failed login attempts the errorLabel text is set.
+                if (failedLoginAttempts >= 3){
+                    errorLabel.setText("Maximum attempts reached! Try again later");
+                }
+                else {
+                    errorLabel.setText("Invalid username or password. Remaining attempts: " +
+                            (3-failedLoginAttempts));
+                }
             }
         });
 
 
         /// Optional Challenge -->
+        /// Label and button to take us to the registration page
         Label createAccountLabel = new Label("Don't have an account?");
         createAccountLabel.setTextFill(Color.LIGHTGRAY);
-        createAccountLabel.setFont(Font.font("Comic sans ms",FontWeight.NORMAL,18));
+        createAccountLabel.setFont(Font.font("Comic sans ms",FontWeight.NORMAL,16));
 
         Button signupButton = new Button("Sign up here");
-        signupButton.setStyle("-fx-font-size: 18px;" +
+        signupButton.setStyle("-fx-font-size: 16px;" +
                 "-fx-font-family: 'Comic Sans MS';" +
                 "-fx-background-color: 'transparent';" +
                 "-fx-text-fill: 'lightgray';"+
-                "-fx-underline: true;");
+                "-fx-underline: true;"
+        );
 
-        signupButton.setOnAction(action -> stage.setScene(new Scene(getRegistrationPage(),600,700)));
 
-        /// Adding all the nodes to the Scene
-        loginPageLayout.getChildren().addAll(view,welcomeText,username,usernameField,password,passwordField,loginButton,errorLabel,createAccountLabel,signupButton);
+        /// Sets the scene to the registration page
+        signupButton.setOnAction(action -> stage.setScene(new Scene(getRegistrationPage(),600,750)));
+
+        /// Adding all the nodes to the pane (scene)
+        loginPageLayout.getChildren()
+                .addAll(view,welcomeText,username,usernameField,password,passwordField,loginButton,errorLabel,createAccountLabel,signupButton);
 
         loginPageLayout.setAlignment(Pos.CENTER);
         return loginPageLayout;
     }
 
 
+    /// Page set to scene when valid inputs are provided
     public Pane getWelcomePage() {
-        VBox welcomePageLayout = new VBox(30);
-
+        VBox welcomePageLayout = new VBox(20);
         welcomePageLayout.setAlignment(Pos.CENTER);
         welcomePageLayout.setStyle("-fx-background-image: url('file:images/NavyBlueSolid.png');"
                 + "-fx-background-size: cover;"
-                + "-fx-background-position: center center;");
+                + "-fx-background-position: center center;"
+        );
+
 
         Image image = new Image("file:images/artemgus2.png");
         ImageView view = new ImageView(image);
-        view.setFitWidth(250);
+        view.setFitWidth(240);
         view.setPreserveRatio(true);
 
         Label welcomeLabel = new Label("Welcome, "+ currentUser + "!");
-        welcomeLabel.setFont(Font.font("Comic sans ms",FontWeight.BOLD,23));
+        welcomeLabel.setFont(Font.font("Comic sans ms",FontWeight.BOLD,22));
         welcomeLabel.setTextFill(Color.WHITE);
 
+        /// How intellij helps styling
         Button logoutButton = new Button("Logout");
         logoutButton.setStyle("-fx-font-size: 22px;" +
                 "-fx-font-family: 'Comic Sans MS';" +
-                "-fx-background-color: 'red';" +
-                "-fx-text-fill: 'white';");
-        logoutButton.setOnAction(action -> stage.setScene(new Scene(getLoginPage(),600,700)));
+                "-fx-background-color: rgba(236,49,78,0.94);" +
+                "-fx-text-fill: 'white';"
+        );
 
+        /// logout button sets the scene back to login page and resets failed attempts.
+        logoutButton.setOnAction(action -> {
+            failedLoginAttempts = 0;
+            stage.setScene(new Scene(getLoginPage(),600,750));
+        });
+
+        ///adding the nodes to the pane
         welcomePageLayout.getChildren().addAll(view, welcomeLabel,logoutButton);
         return welcomePageLayout;
     }
 
 
-    /// Optional -->
+    /// Optional Challenge-->
+    /// This pane is returned when signup button is pressed.
 
     public Pane getRegistrationPage() {
 
         VBox registrationPageLayout = new VBox(13);
-        // TODO 2: Add an artemgus logo as image to the Login page
+        registrationPageLayout.setAlignment(Pos.CENTER);
+
         Image image = new Image("file:images/artemgus3.png");
         ImageView view = new ImageView(image);
         view.setFitWidth(200);
         view.setPreserveRatio(true);
 
-        // TODO 3: Add the welcome text to the login page
 
         Label welcomeText = new Label("Create your account");
         welcomeText.setFont(Font.font("Comic sans ms",FontWeight.BLACK,25));
         welcomeText.setTextFill(Color.WHITE);
-
-        // TODO 4: Add the username and password fields and the login button to the login page
 
         Label name = new Label("Name:");
         name.setTextFill(Color.WHITE);
@@ -195,83 +222,92 @@ public class SealTemisApp {
         passField.setFont(Font.font("Comic sans ms", FontWeight.SEMI_BOLD,18));
         passField.setPromptText("Enter your password");
 
+
         Button createAccountButton = new Button("Create Account");
         createAccountButton.setStyle("-fx-font-size: 20px;" +
                 "-fx-font-family: 'Comic Sans MS';" +
-                "-fx-background-color: #0000EE;" +
-                "-fx-text-fill: 'white';");
+                "-fx-background-color: #2828ed;" +
+                "-fx-text-fill: 'white';"
+        );
 
-        Label errorLabel = new Label("");
+        Label errorLabel = new Label();
         errorLabel.setTextFill(Color.RED);
-        errorLabel.setFont(Font.font("Comic sans ms",FontWeight.BOLD,18));
+        errorLabel.setFont(Font.font("Comic sans ms",FontWeight.SEMI_BOLD,18));
 
         //Helpful CSS styling for the login page background:
         registrationPageLayout.setStyle("-fx-background-image: url('file:images/NavyBlueSolid.png');"
                 + "-fx-background-size: cover;"
-                + "-fx-background-position: center center;"+
-                "-fx-padding: 70;");
+                + "-fx-background-position: center center;"
+                + "-fx-padding: 70;"
+        );
 
-        // TODO 5: Implement the scene change when the createAccount button is clicked
 
+        /// Logic for the create account button -->
         createAccountButton.setOnAction(action -> {
-
+            /// Runs when no input field is empty (Could be more efficient)
             if(!textName.getText().isEmpty() && !textUsername.getText().isEmpty() && !passField.getText().isEmpty()) {
                 /// Resetting the failed login attempts
                 failedLoginAttempts = 0;
+                /// Puts the given username and password to the users Map.
                 users.putIfAbsent(textUsername.getText(), passField.getText());
                 currentUser = textName.getText();
-                stage.setScene(new Scene(getAccountCreatedPage(), 600, 700));
+                stage.setScene(new Scene(getAccountCreatedPage(), 600, 750));
             }
             else{
-                errorLabel.setText("Invalid Input! Please Retry.");
+                errorLabel.setText("Invalid Input! Fields cannot be Empty.");
             }
         });
 
+
         Button goBack = new Button("Go back");
-        goBack.setStyle("-fx-font-size: 18px;" +
+        goBack.setStyle("-fx-font-size: 16px;" +
                 "-fx-font-family: 'Comic Sans MS';" +
                 "-fx-background-color: 'transparent';" +
                 "-fx-text-fill: 'lightgray';"+
-                "-fx-underline: true;");
+                "-fx-underline: true;"
+        );
 
-        goBack.setOnAction(action -> stage.setScene(new Scene(getLoginPage(),600,700)));
+        /// sets scene back to login page WITHOUT resetting the failed attempt
+        goBack.setOnAction(action -> stage.setScene(new Scene(getLoginPage(),600,750)));
 
         /// Adding all the nodes to the Scene
-        registrationPageLayout.getChildren().addAll(view,welcomeText,name,textName,username,textUsername,password,passField,createAccountButton,errorLabel,goBack);
+        registrationPageLayout.getChildren()
+                .addAll(view,welcomeText,name,textName,username,textUsername,password,passField,createAccountButton,errorLabel,goBack);
 
-        registrationPageLayout.setAlignment(Pos.CENTER);
         return registrationPageLayout;
     }
 
 
+    /// This is page is accessed when a new account is successfully created
     public Pane getAccountCreatedPage() {
 
         VBox accountCreatedPageLayout = new VBox(20);
-
         accountCreatedPageLayout.setAlignment(Pos.CENTER);
+
         accountCreatedPageLayout.setStyle("-fx-background-image: url('file:images/NavyBlueSolid.png');"
                 + "-fx-background-size: cover;"
                 + "-fx-background-position: center center;"+
-                "-fx-padding: 70;");
+                "-fx-padding: 70;"
+        );
 
         Image image = new Image("file:images/artemgus4.png");
         ImageView view = new ImageView(image);
         view.setFitWidth(230);
         view.setPreserveRatio(true);
 
-        Label welcomeLabel = new Label("Account successfully created, "+ currentUser + "!");
-        welcomeLabel.setFont(Font.font("Comic sans ms",FontWeight.BOLD,20));
-        welcomeLabel.setTextFill(Color.WHITE);
+        Label accountCreatedLabel = new Label("Account successfully created, "+ currentUser + "!");
+        accountCreatedLabel.setFont(Font.font("Comic sans ms",FontWeight.SEMI_BOLD,20));
+        accountCreatedLabel.setTextFill(Color.WHITE);
 
-        Button logoutButton = new Button("Back to Login");
-        logoutButton.setStyle("-fx-font-size: 20px;" +
+        Button backToLoginButton = new Button("Back to Login");
+        backToLoginButton.setStyle("-fx-font-size: 20px;" +
                 "-fx-font-family: 'Comic Sans MS';" +
-                "-fx-background-color: #00C0FF;" +
+                "-fx-background-color: #1195ef;" +
                 "-fx-text-fill: 'white';");
 
-        logoutButton.setOnAction(action -> stage.setScene(new Scene(getLoginPage(),600,700)));
+        backToLoginButton.setOnAction(action -> stage.setScene(new Scene(getLoginPage(),600,750)));
 
-        accountCreatedPageLayout.getChildren().addAll(view, welcomeLabel,logoutButton);
+        accountCreatedPageLayout.getChildren().addAll(view, accountCreatedLabel,backToLoginButton);
         return accountCreatedPageLayout;
     }
 }
